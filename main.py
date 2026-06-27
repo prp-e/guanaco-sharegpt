30ed3a88521d8078767aa27f389a17493cb17d79
Muhammadreza Haghiri <haghiri75@gmail.com>
2026-06-27 09:12:17 +0330

    a function to convert it to acceptable format for the most of chat models

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 from datasets import load_dataset
 
+def convert_to_sharegpt(example):
+    text = example['text']
+    if "### Human:" not in text or "### Assistant:" not in text:
+        return {"conversations": []}
+      
+    parts = text.split("### Human:")
+
+    
+    content_parts = parts[1].split("### Assistant:")
+    
+    user_content = content_parts[0].strip()
+    assistant_content = content_parts[1].strip()
+    
+    return {
+        "conversations": [
+            {
+                "role" : "user",
+                "content" : user_content
+            },
+            {
+                "role" : "assistant",
+                "content" : assistant_content
+            }
+        ]
+    }
+
 ds = load_dataset("timdettmers/openassistant-guanaco", split="train")
